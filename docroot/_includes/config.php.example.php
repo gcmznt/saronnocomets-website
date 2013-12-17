79a582ef472f7cee70e6fa0c1857656da2a7962d
Giacomo Zinetti <giacomo.zinetti@giko.it>
2013-12-17 22:15:15 +0100

    removed old files

--- docroot/_includes/config.php.example.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $silex['debug'] = false;
-    $context['ga_account'] = 'your_analytics_id';
-
-    define('DB_HOST',           'hostname');
-    define('DB_USER',           'username');
-    define('DB_PASSWORD',       'password');
-    define('DB_NAME',           'database');
--- docroot/_includes/db_connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-	require_once(__DIR__ . '/config.php');
-
-	mysql_connect(DB_HOST, DB_USER, DB_PASSWORD) or die ('Errore di connessione al db');
-	mysql_select_db(DB_NAME) or die ('Errore di selezione del db');
